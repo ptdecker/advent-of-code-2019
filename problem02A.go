f1b40b834b265cec4740ec407f89caaebafd71d0
P. Todd Decker <ptdecker@mac.com>
2019-12-04 00:13:34 -0600

    Refactored out virtual machine

--- problem02A.go
+++ problem02A.go
@@ -73,12 +73,104 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 )
 
-type memory []int
+// VM a virtual machine that can load and run Intcode
+type VM []int // VM's memory
+
+// Size returns the current size of the memory in the VM
+func (vm VM) Size() int {
+	return len(vm)
+}
+
+// Write attempts to write a value to the VM's memory
+func (vm VM) Write(address, val int) {
+	if address < 0 {
+		log.Fatalf("attempt to write to a negative address of %d", address)
+	}
+	if address > vm.Size() {
+		log.Fatalf("attempt to write to address %d but memory stops at %d", address, vm.Size())
+	}
+	vm[address] = val
+}
+
+// Read attempts to retreive a value from VM's memory
+func (vm VM) Read(address int) int {
+	if address < 0 {
+		log.Fatalf("attempt to read to a negative address of %d", address)
+	}
+	if address > vm.Size() {
+		log.Fatalf("attempt to read from address %d but memory stops at %d", address, vm.Size())
+	}
+	return vm[address]
+}
+
+// Load attempts to load VM's memory with Intcode from a file
+func (vm VM) Load(fileName string) (VM, error) {
+
+	// Open data file containing a program
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("cannot load memory from %s: %v", fileName, err)
+	}
+	defer file.Close()
+
+	// Read data file containing the program and load it into core memory
+	scanner := bufio.NewScanner(file)
+	scanner.Split(scanCommas)
+	address := 0
+	for scanner.Scan() {
+		val, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			return nil, fmt.Errorf("at address %d: %v", address, val)
+		}
+		vm = append(vm, val)
+		address++
+	}
+
+	return vm, nil
+}
+
+// add implments the 'add' opcode for the VM
+func (vm VM) add(termAddress1, termAddress2, resultAddress int) {
+	vm.Write(resultAddress, vm.Read(termAddress1)+vm.Read(termAddress2))
+}
+
+// mul implements the 'mul' opcode for the VM
+func (vm VM) mul(termAddress1, termAddress2, resultAddress int) {
+	vm.Write(resultAddress, vm.Read(termAddress1)*vm.Read(termAddress2))
+}
+
+// Run attempts to execute the loaded Intcode program in the VM
+func (vm VM) Run() error {
+	if vm.Size() == 0 {
+		return fmt.Errorf("no program loaded")
+	}
+	ip := 0 // instruction pointer
+execLoop:
+	for {
+		opcode := vm.Read(ip)
+		switch opcode {
+		case 1: // addition
+			vm.add(vm.Read(ip+1), vm.Read(ip+2), vm.Read(ip+3))
+		case 2: // multiplication
+			vm.mul(vm.Read(ip+1), vm.Read(ip+2), vm.Read(ip+3))
+		case 99: // halt
+			break execLoop
+		default:
+			return fmt.Errorf("Invalid opcode %v encountered at position %v", opcode, ip)
+		}
+		ip = ip + 4
+		if ip > vm.Size() {
+			return fmt.Errorf("no halt instruction occured before end of memory")
+		}
+	}
+	return nil
+}
 
 // A helper function that complies with the scanner function needed by scanner.Split
 func scanCommas(data []byte, atEOF bool) (advance int, token []byte, err error) {
@@ -97,63 +189,20 @@ func scanCommas(data []byte, atEOF bool) (advance int, token []byte, err error) 
 	return 0, nil, nil
 }
 
-func load(fileName string) ([]int, error) {
-
-	m := []int{}
-
-	// Open data file containing a program
-	file, err := os.Open(fileName)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	// Read data file containing the program and load it into core memory
-	scanner := bufio.NewScanner(file)
-	scanner.Split(scanCommas)
-	position := 0
-	for scanner.Scan() {
-		opcode, err := strconv.Atoi(scanner.Text())
-		if err != nil {
-			return nil, err
-		}
-		m = append(m, opcode)
-		position++
-	}
-
-	return m, nil
-}
-
 func problem02A(fileName string) int {
 
-	m, err := load(fileName)
+	vm, err := new(VM).Load(fileName)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	m[1] = 12
-	m[2] = 2
+	vm.Write(1, 12)
+	vm.Write(2, 2)
 
-	pos := 0
-execLoop:
-	for {
-		if pos > len(m) {
-			log.Fatalf("Overflow")
-		}
-		opcode := m[pos]
-		switch opcode {
-		case 1: // addition
-			m[m[pos+3]] = m[m[pos+1]] + m[m[pos+2]]
-		case 2: // multiplication
-			m[m[pos+3]] = m[m[pos+1]] * m[m[pos+2]]
-		case 99: // halt
-			break execLoop
-		default:
-			log.Fatalf("Invalid opcode %v encountered at position %v", opcode, pos)
-		}
-		pos = pos + 4
-
+	err = vm.Run()
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	return m[0]
+	return vm.Read(0)
 }
